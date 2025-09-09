6f5254bc8c13b12d8a2b66e5ebebf68ad2f9d32a
“Clockwork” <alex@clockwork.gr>
2025-09-09 12:37:57 +0300

    chore: run example

--- src/example.ts
+++ src/example.ts
@@ -15,7 +15,7 @@ import {
  * blocks, validators, transactions, and network status.
  */
 const init = async () => {
-  const client = await connectTm2("https://rpc.test8.testnets.gno.land/");
+  const client = await connectTm2("http://localjost:26657");
 
   console.log(util.inspect(await client.abciInfo(), {
     depth: null,
